1127ec657df1d48b5aa6773f152352048f4b086a
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-06-23 20:02:04 +0200

    Updaetd StartPage.

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
 import { ROUTES } from "@/constants";
@@ -29,10 +30,16 @@ const Start = (): JSX.Element => {
       </div>
 
       <div className="flex flex-col items-center justify-center space-y-6 md:space-y-10">
-        <SparklesText
-          text="Invite Me!"
-          className="relative z-20 text-center text-3xl font-bold text-white md:text-7xl lg:text-6xl"
-        />
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3, ease: "easeInOut" }}
+        >
+          <SparklesText
+            text="Invite Me!"
+            className="relative z-20 text-center text-3xl font-bold text-white md:text-7xl lg:text-6xl"
+          />
+        </motion.span>
 
         <BlurIn
           word="Create a cool invitation for any occasions."
@@ -40,16 +47,22 @@ const Start = (): JSX.Element => {
           className="relative z-20 text-balance text-center text-xl font-light text-white md:text-2xl lg:text-2xl"
         />
 
-        <Button
-          variant="outline"
-          size="lg"
-          className="relative z-20"
-          onPress={onStartButtonClick}
-          type="button"
-          aria-label="Start"
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 1.5 }}
         >
-          Start
-        </Button>
+          <Button
+            variant="outline"
+            size="lg"
+            className="relative z-20"
+            onPress={onStartButtonClick}
+            type="button"
+            aria-label="Start"
+          >
+            Start
+          </Button>
+        </motion.span>
       </div>
     </div>
   );
