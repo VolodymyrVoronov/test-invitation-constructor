931c74376fb4d03b501b6db5af05787715428ed6
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-06-26 19:48:39 +0200

    Updated components.

--- src/components/SaveButton/SaveButton.tsx
+++ src/components/SaveButton/SaveButton.tsx
@@ -1,5 +1,6 @@
 import { Save } from "lucide-react";
 import { memo } from "react";
+import { Text } from "react-aria-components";
 
 import { SaveFormat } from "@/types";
 
@@ -12,31 +13,37 @@ const SaveButton = memo((): JSX.Element => {
   };
 
   return (
-    <MenuTrigger>
-      <Button
-        aria-label="Save"
-        className="w-full"
-        variant="outline"
-        type="button"
-      >
-        <Save className="mr-2 size-5" />
-        Save as
-        <span className="sr-only">Save as</span>
-      </Button>
-      <MenuPopover className="min-w-[10rem]">
-        <Menu>
-          <MenuItem id="pdf" onAction={() => onSaveButtonClick("pdf")}>
-            PDF
-          </MenuItem>
-          <MenuItem id="png" onAction={() => onSaveButtonClick("png")}>
-            PNG
-          </MenuItem>
-          <MenuItem id="jpeg" onAction={() => onSaveButtonClick("jpeg")}>
-            JPEG
-          </MenuItem>
-        </Menu>
-      </MenuPopover>
-    </MenuTrigger>
+    <div className="grid w-full justify-items-center gap-1">
+      <MenuTrigger>
+        <Button
+          aria-label="Save"
+          className="w-full"
+          variant="outline"
+          type="button"
+        >
+          <Save className="mr-2 size-5" />
+          Save as
+          <span className="sr-only">Save as</span>
+        </Button>
+        <MenuPopover className="min-w-[10rem]">
+          <Menu>
+            <MenuItem id="pdf" onAction={() => onSaveButtonClick("pdf")}>
+              PDF
+            </MenuItem>
+            <MenuItem id="png" onAction={() => onSaveButtonClick("png")}>
+              PNG
+            </MenuItem>
+            <MenuItem id="jpeg" onAction={() => onSaveButtonClick("jpeg")}>
+              JPEG
+            </MenuItem>
+          </Menu>
+        </MenuPopover>
+      </MenuTrigger>
+
+      <Text slot="description" className="text-sm text-muted-foreground">
+        pdf | png | jpeg
+      </Text>
+    </div>
   );
 });
 
